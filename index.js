7ada0491f41b5422d541a23790d8ffe73360ed0a
jhon_macias@hotmail.es <jhonmacias@Jhons-MacBook-Pro.local>
2024-01-02 22:39:45 +0100

    corrigiendo fallo PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Aplicacion corriendo en puerto:" + PORT);
 });
