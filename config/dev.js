0164e639daf3ce3c23a0c0f83093407a40850275
jhon_macias@hotmail.es <jhonmacias@Jhons-MacBook-Pro.local>
2024-01-02 23:01:46 +0100

    nueva actualizacion

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,5 @@
-// dev.js
 const devConfig = {
-  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || "defaultDevKey",
 };
 
 module.exports = devConfig;
